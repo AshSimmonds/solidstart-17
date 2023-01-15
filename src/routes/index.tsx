382cdf97430c1d065c11b368f60d82994e7d4eb7
ashsimmonds <ashsimmonds@gmail.com>
2023-01-15 18:39:24 +0000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -45,9 +45,6 @@ const Home: VoidComponent = () => {
       <h1>SolidStart SSR | Solid @tanstack/query v5</h1>
       <h2>Testing createQuery(fetch) in route vs tRPC</h2>
 
-
-
-
       <div class="text-center mx-auto w-2/3 mt-8 mb-8 flex justify-evenly gap-4">
         <Button.Root
           class="btn btn-secondary btn-outline"
@@ -68,12 +65,8 @@ const Home: VoidComponent = () => {
         </Button.Root>
 
         <div class="text-3xl mt-4">
-          <TextField.Root
-            value={postId().toString()}
-            onValueChange={setPostId}
-          ><TextField.Input
-              class="input input-primary w-16"
-            />
+          <TextField.Root value={postId().toString()} onValueChange={setPostId}>
+            <TextField.Input class="input input-primary w-16" />
           </TextField.Root>
         </div>
 
@@ -96,9 +89,7 @@ const Home: VoidComponent = () => {
         </Button.Root>
       </div>
 
-
       <div class="mx-auto w-full mt-8 flex justify-evenly gap-4">
-
         <div class=" w-full ">
           <Suspense>
             <For each={queryInRoute.data}>
@@ -156,15 +147,23 @@ const Home: VoidComponent = () => {
         </div>
       </div>
 
-
-
-
-
-
-
       <div class="bg-base-100 p-4 mt-12 mb-4">
-        <h3 class="mt-0">
-          Current problems{" "}
+        <h3 class="mt-0">About this project </h3>
+
+        <CheckboxShowHide showWhat="details">
+          <div>
+            This is basically just the personal SolidStart playground of Ash
+            Simmonds, where I'll be randomly adding more pages of basic example
+            implementations of things, some of which might be good reference
+            material (eventually) as a baseline for adding to other projects.
+          </div>
+          <div>
+            I generally make something "just work", note down the things that
+            are buggy or could be improved into GitHub issues (and sometimes
+            listed below directly), then later on when brain is idle come back
+            and sort them out. Appreciate anyone else jumping in to do it their
+            way.
+          </div>
           <a
             href="https://github.com/AshSimmonds/solidstart-17/issues"
             class="btn text-xs btn-sm btn-secondary btn-outline"
@@ -176,60 +175,95 @@ const Home: VoidComponent = () => {
             />{" "}
             github issues
           </a>
-        </h3>
-
-        <CheckboxShowHide showWhat="background details and problems">
-          <h4>PR's welcome.</h4>
+          <h4 class="mt-8">PR's welcome.</h4>
           <div class="mt-8 text-xl">
             <pre>
               <ul>
-                <li><s>several second delay before rendering</s></li>
                 <li>
-                  <s>getting <code>serverless function has crashed</code> on Vercel
-                    occasionally</s>
+                  <a
+                    href="https://github.com/AshSimmonds/solidstart-17/issues/4"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
+                      class="w-6 float-left mr-2"
+                    />
+                    <s>bunch of</s> some DOM hydration warnings in the dev
+                    console
+                  </a>
                 </li>
-                <li><s>some elements don't render</s></li>
-                <li><a href="https://github.com/AshSimmonds/solidstart-17/issues/4" target="_blank"><img
-                  src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
-                  class="w-6 float-left mr-2"
-                /><s>bunch of</s> some DOM hydration warnings in the dev console</a></li>
                 <li>
-                  <s>clicking <code>prev</code> and <code>next</code> buttons jumps
-                    to top of page</s>
+                  <a
+                    href="https://github.com/AshSimmonds/solidstart-17/issues/9"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
+                      class="w-6 float-left mr-2"
+                    />{" "}
+                    first <code>next</code> button click does nothing
+                  </a>
                 </li>
-                <li><s>harsh page flash on fetching new data</s></li>
-                <li><a href="https://github.com/AshSimmonds/solidstart-17/issues/9" target="_blank"><img
-                  src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
-                  class="w-6 float-left mr-2"
-                /> first <code>next</code> button click does nothing</a></li>
-                <li><a href="https://github.com/AshSimmonds/solidstart-17/issues/10" target="_blank"><img
-                  src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
-                  class="w-6 float-left mr-2"
-                /> doesn't register <code>prev</code> button after a few <code>next</code> clicks</a></li>
-                <li><a href="https://github.com/AshSimmonds/solidstart-17/issues/11" target="_blank"><img
-                  src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
-                  class="w-6 float-left mr-2"
-                /> tRPC doesn't show latest when clicking through fast</a></li>
-                <li><s>Reactivity: 'function should be passed to a tracked scope' warning</s></li>
+                <li>
+                  <a
+                    href="https://github.com/AshSimmonds/solidstart-17/issues/10"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
+                      class="w-6 float-left mr-2"
+                    />{" "}
+                    doesn't register <code>prev</code> button after a few{" "}
+                    <code>next</code> clicks
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/AshSimmonds/solidstart-17/issues/11"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
+                      class="w-6 float-left mr-2"
+                    />{" "}
+                    tRPC doesn't show latest when clicking through fast
+                  </a>
+                </li>
               </ul>
             </pre>
             <h3>Further discussion:</h3>
-            Iterating on conversations on Twitter here:
+
             <div class="mt-1 mb-4">
-              <code class="text-xs">
+              <a
+                href="https://twitter.com/BlueDwarfAsh/status/1613866577971277824"
+                class="text-xs"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/3536/3536464.png"
+                  class="w-6 float-left mr-2"
+                />
                 https://twitter.com/BlueDwarfAsh/status/1613866577971277824
-              </code>
+              </a>
             </div>
-            And on Discord here:
             <div class="mt-1">
-              <code class="text-xs">
+              <a
+                href="https://discord.com/channels/722131463138705510/1063020750472237106/1063020750472237106"
+                class="text-xs"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/5968/5968759.png"
+                  class="w-6 float-left mr-2"
+                />
                 https://discord.com/channels/722131463138705510/1063020750472237106/1063020750472237106
-              </code>
+              </a>
             </div>
           </div>
         </CheckboxShowHide>
       </div>
-
     </Layout>
   );
 };
