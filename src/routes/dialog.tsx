127aee34812e800414a0c18b0ada616a3f411b7a
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-19 14:06:45 +1030

    scaffold out new page

---
+++ src/routes/dialog.tsx
@@ -0,0 +1,24 @@
+import type { VoidComponent } from "solid-js"
+import { Title } from "solid-start"
+import Layout from "~/layouts/Layout"
+
+const DialogPage: VoidComponent = () => {
+    return (
+        <Layout>
+            <Title>Dialog examples and testing</Title>
+            <h1>Dialog examples and testing</h1>
+
+            <div class="text-center mx-auto ">
+
+                <h2>Centered stuff</h2>
+
+                <div>
+                    nothing to see here
+                </div>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default DialogPage
