bae50654170b24539897b03e9a31b5bd9b975384
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-20 11:32:47 +1030

    add source code pro font

--- src/root.tsx
+++ src/root.tsx
@@ -126,6 +126,14 @@ export default function Root() {
           rel="preload"
           as="font"
           type="font/woff2"
+          href="./fonts/source-code-pro-v22-latin-regular.woff2"
+          crossorigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
           href="./fonts/HACKER1984.OTF"
           crossorigin="anonymous"
         />
