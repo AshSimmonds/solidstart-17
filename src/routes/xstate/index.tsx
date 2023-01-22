38df65a571c915e4833efd9b8a93376a863ed7ed
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-23 06:41:08 +1030

    basic reactive interface

--- src/routes/xstate/index.tsx
+++ src/routes/xstate/index.tsx
@@ -2,13 +2,14 @@ import type { VoidComponent } from "solid-js";
 import { Title } from "solid-start"
 import { interpret } from "xstate"
 import Layout from "~/layouts/Layout"
-import { checkoutMachine } from "./checkoutMachine"
+import { checkoutMachine } from "~/deus-ex/checkoutMachine"
 import { useMachine } from "./useMachine"
 import { createSignal } from "solid-js"
+import XCOMInfoPanel from "~/components/XCOMInfoPanel"
 
 const actorHoisted = interpret(checkoutMachine).start()
 
-console.log(`xstate | index.tsx | actor`, actorHoisted)
+console.log(`xstate | index.tsx | actorHoisted`, actorHoisted)
 
 
 
@@ -16,10 +17,16 @@ const XStatePage: VoidComponent = () => {
 
     const actorPage = useMachine(checkoutMachine)
 
-    const [checkoutMachineState, setCheckoutMachineState] = createSignal(actorHoisted.initialState)
+    const [theState, setTheState] = createSignal(actorHoisted.initialState)
 
-    const stateValue = () => checkoutMachineState().value.toString()
+    // const stateValue = () => state().value.toString()
 
+
+
+    actorHoisted.onTransition((newState) => {
+        console.log(`xstate | index.tsx | actorHoisted.onTransition | state`, newState)
+        setTheState(newState)
+    })
 
 
 
@@ -29,10 +36,52 @@ const XStatePage: VoidComponent = () => {
             <Title>XState</Title>
             <h1>XState</h1>
 
-            <h2><code>stateValue():</code>{stateValue()}</h2>
+            <h2><code>theState().value:</code>{theState().value.toString()}</h2>
 
 
+            {theState().value === "cart" && (
+                <XCOMInfoPanel>
+                    <h3>Cart</h3>
 
+                    <button class="btn btn-primary" onClick={() => actorHoisted.send("CHECKOUT")}>NEXT</button>
+                </XCOMInfoPanel>
+            )}
+
+            {theState().value === "shipping" && (
+                <XCOMInfoPanel>
+                    <h3>Shipping</h3>
+
+                    <input class="input input-bordered" type="text" placeholder="VIP code"
+                        value={theState().context.vipCode || ""}
+                        data-xstate-event="ENTER_VIP_CODE"
+                        onChange={(e) => actorHoisted.send({ 
+                            type: "ENTER_VIP_CODE", 
+                            value: e.target.value 
+                        })}
+                    />
+
+                    <h4>asdf{theState().context.vipCode}qwer</h4>
+
+                </XCOMInfoPanel>
+            )}
+
+            {theState().value === "contact" && (
+                <XCOMInfoPanel>
+                    <h3>Contact</h3>
+                </XCOMInfoPanel>
+            )}
+
+            {theState().value === "payment" && (
+                <XCOMInfoPanel>
+                    <h3>Payment</h3>
+                </XCOMInfoPanel>
+            )}
+
+            {theState().value === "confirmation" && (
+                <XCOMInfoPanel>
+                    <h3>Confirmation</h3>
+                </XCOMInfoPanel>
+            )}
 
 
 
@@ -50,9 +99,9 @@ const XStatePage: VoidComponent = () => {
 
 
 
-            <h3><code>checkoutMachineState() = createSignal(actorHoisted.initialState)</code></h3>
+            <h3><code>theState() = createSignal(actorHoisted.initialState)</code></h3>
             <pre>
-                {JSON.stringify(checkoutMachineState(), null, 4)}
+                {JSON.stringify(theState(), null, 4)}
             </pre>
 
 
