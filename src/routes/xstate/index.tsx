c8b2b45c68877bee691ac72a4f182bdf3558e060
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-23 05:56:50 +1030

    spaghetti of output on the page

--- src/routes/xstate/index.tsx
+++ src/routes/xstate/index.tsx
@@ -1,16 +1,27 @@
 import type { VoidComponent } from "solid-js";
 import { Title } from "solid-start"
-import { interpret } from "xstate";
+import { interpret } from "xstate"
 import Layout from "~/layouts/Layout"
 import { checkoutMachine } from "./checkoutMachine"
+import { useMachine } from "./useMachine"
+import { createSignal } from "solid-js"
 
-const actor = interpret(checkoutMachine)
+const actorHoisted = interpret(checkoutMachine).start()
 
-console.log(`xstate | index.tsx | actor`, actor)
+console.log(`xstate | index.tsx | actor`, actorHoisted)
 
 
 
 const XStatePage: VoidComponent = () => {
+
+    const actorPage = useMachine(checkoutMachine)
+
+    const [checkoutMachineState, setCheckoutMachineState] = createSignal(actorHoisted.initialState)
+
+    const stateValue = () => checkoutMachineState().value.toString()
+
+
+
 
 
     return (
@@ -18,14 +29,32 @@ const XStatePage: VoidComponent = () => {
             <Title>XState</Title>
             <h1>XState</h1>
 
+            <h2><code>stateValue():</code>{stateValue()}</h2>
 
 
-            <h2>Basic layout</h2>
-            <section>
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-            </section>
+
+
+
+
+            <h3><code>actorHoisted = interpret(checkoutMachine).start()</code></h3>
+            <pre>
+                {JSON.stringify(actorHoisted, null, 4)}
+            </pre>
+
+
+
+            <h3><code>actorPage = useMachine(checkoutMachine)</code></h3>
+            <pre>
+                {JSON.stringify(actorPage, null, 4)}
+            </pre>
+
+
+
+            <h3><code>checkoutMachineState() = createSignal(actorHoisted.initialState)</code></h3>
+            <pre>
+                {JSON.stringify(checkoutMachineState(), null, 4)}
+            </pre>
+
 
 
 
