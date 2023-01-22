c02e9f5ce04a8586bbf67c4d1dae870b08be06dd
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-23 01:08:50 +1030

    install xstate

---
+++ src/routes/xstate/index.tsx
@@ -0,0 +1,29 @@
+import type { VoidComponent } from "solid-js";
+import { Title } from "solid-start"
+import Layout from "~/layouts/Layout"
+
+const XStatePage: VoidComponent = () => {
+
+
+    return (
+        <Layout>
+            <Title>XState</Title>
+            <h1>XState</h1>
+
+
+
+            <h2>Basic layout</h2>
+            <section>
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+            </section>
+
+
+
+
+        </Layout>
+    )
+}
+
+export default XStatePage
