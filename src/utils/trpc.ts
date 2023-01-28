228cd2e767a8da2cd50e4e85dd1bc83e2bd35665
Ash Simmonds <ashsimmonds@gmail.com>
2023-01-29 06:14:09 +1030

    update trpc utils

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,21 +1,27 @@
 import { QueryClient } from "@adeora/solid-query";
 import type { IAppRouter } from "~/server/trpc/router/_app";
-import { createTRPCSolid } from "solid-trpc";
+import { createTRPCSolidStart } from "solid-trpc";
 import { httpBatchLink } from "@trpc/client";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
+
   if (process.env.NODE_ENV === "production")
-    return "https://solidstart.g0.wtf";
+  return "https://solidstart.g0.wtf";
+
   return `http://localhost:${process.env.PORT ?? 5173}`;
 };
 
-export const trpc = createTRPCSolid<IAppRouter>();
-export const client = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: `${getBaseUrl()}/api/trpc`,
-    }),
-  ],
+export const trpc = createTRPCSolidStart<IAppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    };
+  },
 });
+
 export const queryClient = new QueryClient();
