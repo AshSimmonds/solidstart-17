11065291146fba785013e4acfed33e6e3aefd65f
AshSimmonds <ashsimmonds@gmail.com>
2023-02-19 22:24:38 +1300

    usesession not create

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -53,44 +53,42 @@ export default HomePage
 
 
 
-
-const AuthShowcase: VoidComponent = () => {
-    const sessionData = createSession();
-
-    return (
-        <Layout>
-            <div class="flex flex-col items-center justify-center gap-4">
-                <p class="text-center text-2xl text-white">
-                    {sessionData() && <span>Logged in as {sessionData()?.user?.display_name}</span>}
-                </p>
-                <button
-                    class="btn"
-                    onClick={() => void signIn("discord")}
-                >
-                    "Sign in"
-                </button>
-
-                <button
-                    class=""
-                    onClick={
-                        () => void signOut()
-                    }
-                >
-                    "Sign out"
-                </button>
-
-                <h3>user metadata</h3>
-                <pre class="">
-                    {JSON.stringify(sessionData()?.user, null, 4)}
-                </pre>
-
-            </div>
-        </Layout>
-    );
-};
-
-const createSession = () => {
+const useSession = () => {
     return createServerData$(async (_, event) => {
         return await getSession(event.request, authOpts);
     });
 };
+
+const AuthShowcase: VoidComponent = () => {
+    const sessionData = useSession();
+
+    return (
+        <div class="flex flex-col items-center justify-center gap-4">
+            <p class="text-center text-2xl text-white">
+                {sessionData() && <span>Logged in as {sessionData()?.user?.display_name}</span>}
+            </p>
+            <button
+                class="btn"
+                onClick={() => void signIn("discord")}
+            >
+                "Sign in"
+            </button>
+
+            <button
+                class=""
+                onClick={
+                    () => void signOut()
+                }
+            >
+                "Sign out"
+            </button>
+
+            <h3>user metadata</h3>
+            <pre class="">
+                {JSON.stringify(sessionData()?.user, null, 4)}
+            </pre>
+
+        </div>
+    );
+};
+
