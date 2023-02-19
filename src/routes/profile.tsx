5f255d79fc1b75152f69e4e835be7315f78382ed
AshSimmonds <ashsimmonds@gmail.com>
2023-02-19 18:51:31 +1300

    Update profile.tsx

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,90 +1,164 @@
 import type { VoidComponent } from "solid-js";
-import { For } from "solid-js";
-import { createSignal } from "solid-js"
-import { Title } from "solid-start"
+import { Switch, Match, Show, For } from "solid-js"
+import { A, Title } from "solid-start"
+import { trpc } from "~/utils/trpc"
+import { createServerData$ } from "solid-start/server"
 import Layout from "~/layouts/Layout"
-import Card from "~/components/Card"
+import { createSignal } from "solid-js"
+import XCOMOperationBriefing from "~/components/XCOMOperationBriefing"
+import { getSession } from "@auth/solid-start"
+import { authOpts } from "./api/auth/[...solidauth]"
+import LogInOutButton from "~/components/LogInOutButton"
+import Avatar from "~/components/Avatar";
 
-const BlankPage: VoidComponent = () => {
-
-    const [cardCownt, setCardCount] = createSignal(9)
 
 
+const useSession = () => {
+    return createServerData$(
+        async (_, { request }) => {
+            return await getSession(request, authOpts)
+        },
+        { key: () => ["auth_user"] }
+    )
+}
 
 
+
+const ProfilePage: VoidComponent = () => {
+    const session = useSession()
+    const user = () => session()?.user
+
+    // if (!user) {
+    //     return (
+    //         <Layout>
+    //             <Title>Profile</Title>
+    //             <h1>Profile</h1>
+    //             <LogInOutButton />
+    //         </Layout>
+    //     )
+    // }
+
+    // const res = trpc.exampleRouter.secret.useQuery(undefined, {
+    //     get enabled() {
+    //         return !!user()
+    //     },
+    // })
+
+    const [discordUser, setDiscordUser] = createSignal(trpc.discordRouter.getCurrentUser.useQuery())
+
+    const [discordActiveThreads, setDiscordActiveThreads] = createSignal(trpc.discordRouter.getActiveThreads.useQuery())
 
 
     return (
         <Layout>
-            <Title>Blank</Title>
-            <h1>Empty</h1>
+            <Title>Profile</Title>
+            <h1>Profile {user() && `: ${user()?.displayName}`}</h1>
+
+            <LogInOutButton class="float-right" />
+
+            <Avatar />
 
 
-
-            <h2>Basic layout</h2>
-            <section>
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-            </section>
-
-
-
-
-            <h2>Centered stuff</h2>
-            <section class="mx-auto w-full md:w-4/5 lg:w-2/3 xl:w-1/2 ">
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-            </section>
+            <div>
+                <Switch>
+                    <Match when={user()}>
 
 
 
 
-            <h2>Neat card layout</h2>
 
-            <div class="input-group mx-auto w-56">
-                <button class="btn btn-primary" onClick={() => setCardCount(cardCownt() - 1)}>-1</button>
-                <input class="input input-ghost input-bordered w-36 mb-8 " type="number" value={cardCownt()} onChange={(e) => setCardCount(parseInt(e.currentTarget.value))} />
-                <button class="btn btn-primary" onClick={() => setCardCount(cardCownt() + 1)}>+1</button>
+                        <XCOMOperationBriefing
+                            title="Discord active threads"
+                        >
+
+                            <Show when={discordActiveThreads().data}>
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <th>
+                                                #
+                                            </th>
+                                            <th>
+                                                title
+                                            </th>
+                                            <th>
+                                                channel
+                                            </th>
+                                            <th>
+                                                created
+                                            </th>
+                                            <th>
+                                                posts
+                                            </th>
+                                        </tr>
+                                        <For each={discordActiveThreads().data}>
+                                            {(theThread: {
+                                                id: string,
+                                                guild_id: string,
+                                                name: string,
+                                                channelName: string,
+                                                last_message_id: string,
+                                                member_ids_preview: string[],
+                                                message_count: number,
+                                                owner_id: string,
+                                                parent_id: string,
+                                                thread_metadata: {
+                                                    archived: boolean,
+                                                    create_timestamp: string,
+                                                    locked: boolean,
+                                                },
+                                                total_message_sent: number,
+                                            }, threadIndex) => (
+                                                <tr>
+                                                    <td>
+                                                        {threadIndex() + 1}
+                                                    </td>
+                                                    <td>
+                                                        <a href={`https://discord.com/channels/${theThread.guild_id}/${theThread.id}/${theThread.last_message_id}`} target="_blank">{theThread.name}</a>
+                                                    </td>
+                                                    <td>
+                                                        {theThread.channelName}
+                                                    </td>
+                                                    <td class="font-mono text-sm whitespace-nowrap">
+                                                        {new Date(theThread.thread_metadata.create_timestamp).toLocaleString()}
+                                                    </td>
+                                                    <td>
+                                                        {theThread.message_count}
+                                                    </td>
+                                                </tr>
+                                            )}
+                                        </For>
+                                    </tbody>
+                                </table>
+                            </Show>
+
+
+                            {/* <pre>
+                                {JSON.stringify(discordActiveThreads().data, null, 4)}
+                            </pre> */}
+
+
+                        </XCOMOperationBriefing>
+
+
+
+
+
+
+
+                    </Match>
+                </Switch>
             </div>
 
-            <section class="w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8 xl:grid-cols-3 2xl:grid-cols-4">
-
-                <For each={Array(cardCownt())}>
-                    {() => (
-                        <Card
-                            title="Card title"
-                            subtitle="Card subtitle"
-                            img="https://placeimg.com/400/250/arch"
-                            imgBottom={false}
-                            imgFull={false}
-                            cardUrl=""
-                            buttonText=""
-                            buttonUrl=""
-                            buttonClass=""
-                            rounded={false}
-                            outline={false}
-                            hoverZoom={true}
-                            hoverHighlight={true}
-                            class=""
-                        >
-                            <div class="">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </div>
-                        </Card>
-                    )}
-                </For>
-
-            </section>
-
-
-
-
-
+            <h2>
+                user() metadata
+            </h2>
+            <pre>
+                {JSON.stringify(user(), null, 4)}
+            </pre>
 
         </Layout>
     )
 }
 
-export default BlankPage
+export default ProfilePage
